3bc579a7418d4f84fd774676f0d5e74bb445234d
Pavlovaonline <katekeehl@yandex.ru>
2022-01-31 07:28:30 +0300

    Initial commit

---
+++ app/src/main/java/com/gb/group1919_lesson02_v0/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gb.group1919_lesson02_v0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
